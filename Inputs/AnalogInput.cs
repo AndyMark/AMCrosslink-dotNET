919fd4dba388885f21d59215ee232a8d1f2312b6
Greg McCoy <greg@andymark.com>
2012-08-20 15:51:03 -0400

    Added voltage properties to AnalogInput

--- Inputs/AnalogInput.cs
+++ Inputs/AnalogInput.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace com.andymark.crosslink
 {
+    /// <summary>
+    /// Represents an analog input on the Canipede.
+    /// </summary>
     public class AnalogInput
     {
         private Toucan toucan;
@@ -15,11 +18,37 @@ namespace com.andymark.crosslink
             this.channel = channel;
         }
 
-        public UInt16 Value
+        /// <summary>
+        /// Raw data received from the Canipede.
+        /// </summary>
+        public UInt16 Raw
         {
             get
             {
                 return toucan.GetAnalogRaw(channel);
+            }
+        }
+
+        /// <summary>
+        /// Value expressed as a voltage.
+        /// </summary>
+        public double Voltage
+        {
+            get
+            {
+                return ((int)Raw) * 0.0048984375; // raw * 3.3/1024 * ( 5.2 + 10) /  (10)
+            }
+        }
+
+        /// <summary>
+        /// Canipede voltage on analog input 8 when the jumper is in place.
+        /// Compensates for the internal voltage divider.
+        /// </summary>
+        public double BatteryVoltage
+        {
+            get
+            {
+                return ((int)Raw) * 0.02745703125; // raw * 3.3/1024 * ( 70 + 5.2 + 10) /  (10)
             }
         }
     }
