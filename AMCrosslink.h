ed800a4654a3530f1dca202ac232ecd5aa66c43c
Greg McCoy <greg@andymark.com>
2012-08-21 11:41:31 -0400

    Added Doxygen support

---
+++ AMCrosslink.h
@@ -0,0 +1,11 @@
+/*! \mainpage
+ *
+ * \section intro_sec Introduction
+ *
+ * This is a .NET library for use with the Crosslink Robot Control System.
+ * The source code is licensed under a BSD-style open source
+ * license.
+ * 
+ * Click <a href="http://www.andymark.com/product-p/am-0994.htm">here</a> to purchase a Crosslink control system.
+ *
+ */
